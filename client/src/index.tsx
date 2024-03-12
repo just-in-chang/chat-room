3963aa0d06fe99f51c26ae01857df0964d2a6e72
Justin Chang <justin@aptoslabs.com>
2024-03-12 02:11:38 -0700

    remove github pages

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const wallets = [new PetraWallet()];
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+          <App />
+        </AptosWalletAdapterProvider>
+      ),
+    },
+  ],
+  {
+    basename: process.env.PUBLIC_URL,
+  },
+);
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
-);
-root.render(
-  <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
-    <App />
-  </AptosWalletAdapterProvider>,
-);
+).render(<RouterProvider router={router} />);
